3577dbd0aa5ff2ff1665ed617de9176fd4f2822e
Jason Lee <jasonlee@Jasons-MacBook-Pro.local>
2025-02-21 22:28:29 -0500

    Added API routes for frontend

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, jsonify
+from thingsNearMe import get_hidden_gems, get_atlas_obscura_places
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return jsonify({"message": "Hello from Render!"})
+@app.route("/api/get-hidden-gems")
+def api_hidden_gems():
+    return jsonify({"places": get_hidden_gems()})
+
+@app.route("/api/get-atlas-obscura")
+def api_atlas_obscura():
+    return jsonify({"places": get_atlas_obscura_places()})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
